7b882586ec4f7e9f2acdf0b638d18b5e32a771bf
Oscar Giordano <98002522+Crotoconlaptop@users.noreply.github.com>
2023-12-10 07:04:16 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function showCocktailDetails(cocktailName) {
+    // Oculta todos los detalles de los cocteles
+    var cocktailDetails = document.querySelectorAll('.cocktail-details');
+    cocktailDetails.forEach(function(detail) {
+        detail.style.display = 'none';
+    });
+
+    // Muestra los detalles del coctel seleccionado
+    var selectedCocktail = document.getElementById(cocktailName + '-details');
+    selectedCocktail.style.display = 'block';
+}
